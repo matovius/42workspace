047b95caa115b0beb103468b06ddac5d3a6cd2f2
Martin Matovu <matovius@proton.me>
2025-01-07 00:23:55 +0300

    add links object arrays

---
+++ src/lib/scripts/links.ts
@@ -0,0 +1,62 @@
+import type { Link } from './types';
+
+// TODO: Populate general nav links url fields
+export const GeneralNavLinks: Link[] = [
+	{
+		label: 'Spaces',
+		url: '/spaces'
+	},
+	{
+		label: 'Events',
+		url: '/#'
+	},
+	{
+		label: 'Blog',
+		url: '/#'
+	}
+];
+// TODO: Populate extra nav links url fields
+export const ExtraNavLinks: Link[] = [
+	{
+		label: 'Startup Visa Program',
+		url: '/#'
+	},
+	{
+		label: 'Careers',
+		url: '/#'
+	}
+];
+// TODO: Populate social links url fields
+export const SocialLinks: Link[] = [
+	{
+		label: 'Facebook',
+		url: '/#'
+	},
+	{
+		label: 'LinkedIn',
+		url: '/#'
+	},
+	{
+		label: 'Instagram',
+		url: '/#'
+	}
+];
+// TODO: Populate legal links url fields
+export const LegalLinks: Link[] = [
+	{
+		label: 'Code of conduct',
+		url: '/#'
+	},
+	{
+		label: 'Press kit',
+		url: '/#'
+	},
+	{
+		label: 'Privacy policy',
+		url: '/#'
+	},
+	{
+		label: 'Terms and conditions',
+		url: '/#'
+	}
+];

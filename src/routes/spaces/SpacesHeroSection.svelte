f2149d5e5df883ea1bfac7855537a036355dd665
Martin Matovu <matovius@proton.me>
2025-01-09 16:39:01 +0300

    feat: add component

---
+++ src/routes/spaces/SpacesHeroSection.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+</script>
+
+<section id="hero" class="hero-section">
+  <div class="container">
+    <div class="lead">
+      <h1 class="h1">Coworking Spaces at 42workspace</h1>
+      <p class="p">
+        We have various types of coworking spaces, ranging from flexible workspaces for individual
+        entrepreneurs and freelancers to private office spaces for teams.
+      </p>
+    </div>
+  </div>
+</section>
+
+
+<style>
+  /* HERO SECTION */
+  section.hero-section {
+    color: var(--clr-white);
+    min-height: 50vh;
+    min-height: 50lvh;
+    background: var(--clr-oxford-blue);
+  }
+  section.hero-section > div.container {
+    padding-block: 12.5rem 6rem; /* 200px, 96px */
+  }
+  /* Lead */
+  section.hero-section div.lead {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+  section.hero-section h1.h1 {
+    text-align: center;
+  }
+  section.hero-section p.p {
+    text-align: center;
+    max-width: 37.5rem; /* 600px */
+    padding-block-start: 1.5rem; /* 24px */
+  }
+</style>

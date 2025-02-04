9fc030e49b9b8e2b4fa8082d305ef995ae3627fb
Martin Matovu <matovius@proton.me>
2025-02-04 12:38:14 +0300

    feat: add `Duration` and `Space` types

--- src/lib/scripts/types.ts
+++ src/lib/scripts/types.ts
@@ -2,3 +2,15 @@ export type Link = {
 	label: string;
 	url: string;
 };
+
+export type Duration = 'hour' | 'day' | 'month';
+
+export type Space = {
+	name: string;
+	price: number;
+	duration: Duration;
+	url?: string;
+	details?: string;
+	img?: string;
+	imgAlt?: string;
+};
